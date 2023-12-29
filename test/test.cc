148b7b79a454d3516064cf004592a8073c33810f
luckydog <13040588581@163.com>
2023-12-29 21:19:32 +0800

    2023-12-29

---
+++ test/test.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include "contacts.pb.h"
+using namespace std;
+
+
+int main()
+{
+	string peopleInfo;
+	{
+		contacts::PeopleInfo peo;
+		peo.set_name("aaa");
+		peo.set_age(100);
+		// 序列化
+		if(!peo.SerializeToString(&peopleInfo))
+			cout << "peo.SerializeToString() error" << endl;
+
+		cout << "序列化后: " << peopleInfo << endl;
+	}
+
+	{
+		contacts::PeopleInfo peo;
+		if(!peo.ParseFromString(peopleInfo))
+			cout << "peo.ParseFromString() error" << endl;
+
+		cout << "name: " << peo.name() << endl;
+		cout << "age: " << peo.age() << endl;
+	}
+
+	return 0;
+}
